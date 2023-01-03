0dad44ce97b01a8291bf41e50494a14e2142ed22
emmanuel-tientcheu <89963864+emmanuel-tientcheu@users.noreply.github.com>
2023-01-04 00:11:50 +0100

    immeuble

---
+++ Gestion/src/config/IImmeuble.java
@@ -0,0 +1,8 @@
+package config;
+
+public interface IImmeuble {
+    public Immeuble creteImmeuble(Immeuble immeuble);
+    public Immeuble updateImmeuble(Immeuble immeuble);
+    public Immeuble deleteImmeuble(Immeuble immeuble);
+    public Immeuble getImmeuble(int idImmeuble);
+}
