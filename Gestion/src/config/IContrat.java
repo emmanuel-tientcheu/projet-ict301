4819128aa9424c01c2740f07e36b4ae2a871a542
emmanuel-tientcheu <89963864+emmanuel-tientcheu@users.noreply.github.com>
2023-01-04 07:27:00 +0100

    contrat

---
+++ Gestion/src/config/IContrat.java
@@ -0,0 +1,8 @@
+package config;
+
+public interface IContrat {
+    public Contrat createContrat(Contrat contrat);
+    public Contrat updateContrat(Contrat contrat);
+    public Contrat deleteContrat(Contrat contrat);
+    public Contrat getContrat(int idContrat);
+}
