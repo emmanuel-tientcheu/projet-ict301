692ed42e25704566548f4777e718fe28681ccbd7
emmanuel-tientcheu <89963864+emmanuel-tientcheu@users.noreply.github.com>
2023-01-04 07:04:08 +0100

    avocat

---
+++ Gestion/src/config/IAvocat.java
@@ -0,0 +1,10 @@
+package config;
+
+public interface IAvocat {
+    public Avocat createAvocat(Avocat avocat) ;
+    public Avocat updateteAvocat(Avocat avocat) ;
+    public Avocat deleteAvocat(Avocat avocat) ;
+    public Avocat getAvocat(int idAvocat) ;
+
+
+}
