5afd195e5449de9d351d2caf3a7b78c120f27577
konnofuente <konomelifuente@gmail.com>
2023-01-05 14:30:20 +0100

    1st step

---
+++ K-IMMO/src/main/java/com/example/kimmo/ControllerAccount.java
@@ -0,0 +1,126 @@
+package com.example.kimmo;
+
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerAccount implements Initializable {
+    // @FXML private ImageView exit;
+
+    @FXML private Label menu;
+
+    @FXML private Label menuback;
+
+    @FXML
+    private AnchorPane slider;
+
+    @FXML
+    private StackPane contentArea;
+
+    @FXML private Button btnexit;
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+
+        btnexit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                DBUtils.changeover(actionEvent,"accueil.fxml","WELCOME");
+            }
+        });
+
+        menu.setOnMouseClicked(event ->{
+            TranslateTransition slide =new TranslateTransition();
+            slide.setDuration(Duration.seconds(0.5));
+            slide.setNode(slider);
+
+            slide.setToX(0);
+            slide.play();
+
+            slider.setTranslateX(-220);
+
+            slide.setOnFinished((ActionEvent e)->{
+                menu.setVisible(false);
+                menuback.setVisible(true);
+            });
+        });
+
+
+
+        menuback.setOnMouseClicked(event ->{
+            TranslateTransition slide =new TranslateTransition();
+            slide.setDuration(Duration.seconds(0.5));
+            slide.setNode(slider);
+
+            slide.setToX(-220);
+            slide.play();
+
+            slider.setTranslateX(0);
+
+            slide.setOnFinished((ActionEvent e)->{
+                menu.setVisible(true);
+                menuback.setVisible(false);
+            });
+        });
+
+        try{
+            Parent root= FXMLLoader.load(getClass().getResource("welcomeAdmin.fxml"));
+            contentArea.getChildren().removeAll();
+            contentArea.getChildren().setAll(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void exit (ActionEvent actionEvent) throws IOException {
+        System.exit(0);
+    }
+
+    public void dashboard (ActionEvent actionEvent) throws IOException {
+        Parent root= FXMLLoader.load(getClass().getResource("2servicepage.fxml"));
+        contentArea.getChildren().removeAll();
+        contentArea.getChildren().setAll(root);
+    }
+
+    public void promise (ActionEvent actionEvent) throws IOException {
+        Parent root= FXMLLoader.load(getClass().getResource("promise.fxml"));
+        contentArea.getChildren().removeAll();
+        contentArea.getChildren().setAll(root);
+    }
+
+    public void disclaimer (ActionEvent actionEvent) throws IOException {
+        Parent root= FXMLLoader.load(getClass().getResource("disclaimer.fxml"));
+        contentArea.getChildren().removeAll();
+        contentArea.getChildren().setAll(root);
+    }
+
+    public void statistic(ActionEvent actionEvent) throws IOException {
+        Parent root= FXMLLoader.load(getClass().getResource("statistic.fxml"));
+        contentArea.getChildren().removeAll();
+        contentArea.getChildren().setAll(root);
+    }
+
+    public void clinic (ActionEvent actionEvent) throws IOException {
+        Parent root= FXMLLoader.load(getClass().getResource("update_clinic.fxml"));
+        contentArea.getChildren().removeAll();
+        contentArea.getChildren().setAll(root);
+    }
+}
---
+++ K-IMMO/src/main/java/com/example/kimmo/ControllerClientLogin.java
@@ -0,0 +1,92 @@
+package com.example.kimmo;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+//
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+
+public class ControllerClientLogin implements Initializable {
+
+    @FXML private Button btnadd_admin;
+
+    @FXML private Button btn_reset;
+
+    @FXML private Button btnlogin_admin;
+
+    @FXML private Button btnreturn;
+
+    @FXML private TextField tfadmin_mat;
+
+    @FXML private TextField tfadmin_pwd;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //the button that reset the textfield
+        btn_reset.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                tfadmin_pwd.clear();
+                tfadmin_mat.clear();
+
+            }
+        });
+
+        //return to the mainpage2
+        btnreturn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                DBUtils.changeover(actionEvent,"accueil.fxml","WELCOME TO MAIN PAGE");
+            }
+        });
+
+        //permit us to login
+        btnlogin_admin.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+//                DBUtils.changeover(actionEvent,"mainpage2.fxml","WELCOME TO MAIN PAGE");
+//                changehigh(actionEvent, "sliderAdmin_Menu.fxml","ADMINISTRATION");
+
+//                if (tfadmin_mat.getText().trim().isEmpty() || tfadmin_pwd.getText().trim().isEmpty()) {
+//
+//
+//                    Alert alert = new Alert(Alert.AlertType.WARNING);
+//                    alert.setContentText("Please enter the Matricule and password");
+//                    alert.show();
+//
+//                } else {
+//
+//                    try {
+//                        DBUtils.logInUser(actionEvent,tfadmin_mat.getText(),tfadmin_pwd.getText());
+//
+//                    } catch (SQLException e) {
+//                        e.printStackTrace();
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//
+//                }
+            }
+        });
+
+        //goes to the page that permit us to add admin
+//        btnadd_admin.setOnAction(new EventHandler<ActionEvent>() {
+//            @Override
+//            public void handle(ActionEvent actionEvent) {
+//                Alert alert=new Alert(Alert.AlertType.CONFIRMATION);
+//                alert.setContentText("You Be directed to Creat Admin");
+//                alert.show();
+//
+//                DBUtils.changeover(actionEvent,"signup_admin.fxml","SIGN UP PAGE");
+//            }
+//        });
+//
+    }
+}
