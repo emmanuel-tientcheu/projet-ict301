1f5696dc8b1688ecd6dcb51561879df7497578df
emmanuel-tientcheu <89963864+emmanuel-tientcheu@users.noreply.github.com>
2023-01-05 12:53:06 +0100

    desistement

--- Gestion/src/Main.java
+++ Gestion/src/Main.java
@@ -12,10 +12,13 @@ public class Main {
         directeur.getDirecteur(3);*/
         /*Visiter visiter = new Visiter(1,1,new Date(2023-01-03),"tres propre",Visiter.getAll()+1);
         visiter.creationPromesse();*/
-        Promesse promesse = new Promesse(Promesse.getAll()+1,1,1,1,1,0,0,new Date(2023-01-03),300,35);
+      //  Promesse promesse = new Promesse(Promesse.getAll()+1,1,1,1,1,0,0,new Date(2023-01-03),300,35);
       //  promesse.createPromesse(promesse);
-        promesse.updatePromesse(promesse);
-        Visiter visite = null;
+      /*  promesse.updatePromesse(promesse);
+        Visiter visite = null;*/
        // System.out.println("id"+Visiter.getAll());
+     /*   Desistement desistement = new Desistement(Desistement.getAll()+1,1,1,23,new Date(23-01-03),"tres propre",0,1,1,1);
+        desistement.updateDesistement(desistement);*/
+        Desistement.getAll();
     }
 }
